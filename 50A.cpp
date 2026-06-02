2652849529a302ce193ba2a4f7f6754371b46449
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2026-06-02 12:11:43 +0530

    solved 50A

---
+++ 50A.cpp
@@ -0,0 +1,16 @@
+/*
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin>>m>>n;
+    int board = m*n;
+    int dom = board/2;
+    cout<<dom;
+    
+    return 0;
+}
